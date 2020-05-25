98dd7471dbe70d795f3d8c69e9e677768c29dede
Ani Talakhadze <atal15@freeuni.edu.ge>
2020-05-25 23:11:48 +0400

    Added simple implementation for JCount class.

--- JCount.java
+++ JCount.java
@@ -5,14 +5,64 @@
 */
 
 import javax.swing.*;
-import java.awt.event.*;
 
 public class JCount extends JPanel {
+	JTextField field;
+	JLabel label;
+	JButton start, stop;
+	Thread currentWorker;
+
 	public JCount() {
 		// Set the JCount to use Box layout
 		setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 		
 		// YOUR CODE HERE
+		field = new JTextField("100000000");
+		label = new JLabel("0");
+		start = new JButton("Start");
+		start.addActionListener(actionEvent -> JCount.this.onStart());
+		stop = new JButton("Stop");
+		stop.addActionListener(actionEvent -> JCount.this.onStop());
+		this.add(field);
+		this.add(label);
+		this.add(start);
+		this.add(stop);
+		this.add(Box.createVerticalStrut(40));
+		currentWorker = new Worker();
+	}
+
+	public void onStart(){
+		if (currentWorker.isAlive()) currentWorker.interrupt();
+		currentWorker = new Worker();
+		currentWorker.start();
+	}
+
+	public void onStop(){
+		if (currentWorker.isAlive()) currentWorker.interrupt();
+	}
+
+	public void updateLabel(int count){
+		SwingUtilities.invokeLater(() -> label.setText(String.valueOf(count)));
+	}
+
+	class Worker extends Thread{
+		public void run(){
+			int bound = Integer.parseInt(field.getText());
+			for (int i = 0; i < bound; i++) {
+				try{
+					sleep(1);
+				} catch (InterruptedException ex) {
+					System.out.println(ex.getMessage());
+					return;
+				}
+				if (i % 1000 == 0){ updateLabel(i); }
+				if(isInterrupted()) {
+					System.out.println("thread is interrupted");
+					return;
+				}
+			}
+			System.out.println("Thread finished job without interruption");
+		}
 	}
 	
 	static public void main(String[] args)  {
